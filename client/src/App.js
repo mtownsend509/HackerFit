0cc1dac4cabd9ec606f718278da44d8da9925ed5
Emilia Trentham <lavernatrentham@gmail.com>
2023-03-14 23:16:57 -0700

    Install Tailwind CSS and components for login and signup functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,87 @@
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import  Profile  from "./pages/Profile";
-import  CreateRoutine  from "./pages/CreateRoutine";
-import  RoutineLogg  from "./pages/RoutineLogg";
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Footer from "./components/Footer";
+import CreateRoutine from "./pages/CreateRoutine";
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import  CreateRoutine  from "./pages/CreateRoutine";
+// import RoutineLogg from "./pages/RoutineLogg";
 
-import NavBar from "./components/NavBar";
 
-import { useState } from "react";
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [
-    currentPage,
-    setCurrentPage,
-  ] = useState("Profile");
-  const renderPage = () => {
-    switch (currentPage) {
-      case "Profile":
-        return <Profile />
-     
-      case "Create Routine":
-        return <CreateRoutine />
-       
-      case "Routine Logg":
-        return <RoutineLogg />
-      
-      default:
-        return <p>
-          error
-        </p>;
-    }
-  };
+
 
   return (
  
-    <div className="App">
-     
-        <NavBar
-          setCurrentPage={
-            setCurrentPage
-          }
-        />
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <h1 className="text-3xl font-bold underline font-brand">
-          Hello world!
-        </h1>
-  
-        <div>
-          {renderPage()}
-          </div>
-  
+    <ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="">
+          <Routes>
+            <Route 
+              path="/"
+              element={<Profile />}
+            />
+             {/* <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />}
+              /> */}
+                  {/* <Route 
+                path="/me" 
+                element={<Profile />}
+              /> */}
+              {/* <Route 
+                path="/profiles/:username" 
+                element={<Profile />}
+              />
+              <Route 
+                path="/exercises/:exerciseId" 
+                element={<CreateRoutine />}
+              /> */}
+          </Routes>
+        </div>
+        <Footer />
       </div>
-   
+    </Router>
+  </ApolloProvider>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/**/*.{html,js}',
+  content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
