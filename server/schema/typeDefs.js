fd8e1cc22b89c9f4e3bd879de8a26df709671dd5
Carrie Young <carrieky04@gmail.com>
2023-03-16 15:43:51 -0700

    adds typedefs and more resolvers

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,4 +1,48 @@
 const typeDefs = `#graphql
+    type User {
+        _id: _id
+        username: String
+        email: String
+        savedRoutines: [Routine]!
+    }
+
+    type Routine {
+        _id: _id
+        name: String
+        exercises: [Exercise]!
+    }
+
+    type Exercise {
+        _id: _id
+        name: String
+        muscle: String
+        instructions: String
+        reps_sets: String
+    }
+
+    type Auth {
+        _id: ID!
+        user: User
+    }
+
+    type Query {
+        user(username: String!): User
+        exercises(muscle: String!): [Exercise]
+        routines(name: String!): [Routine]
+        routine(routineId: ID!): Routine
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        createRoutine(name: String!): Routine
+        addExercise(routineId: ID!, name: String!): Routine
+        updateExercise(exerciseId: ID!,reps_sets: String!): Exercise
+        deleteRoutine(name: String!): Routine
+        deleteExercise(exerciseId: ID!, routineId: ID!): Exercise
+        
+        
+    }
 
 `
 
