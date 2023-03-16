5d284fff205016216160eda048dc4221b70afacf
Kyle Feldman <flip93wolf@gmail.com>
2023-03-16 14:46:16 -0700

    Added content to NavBar, Renamed page CreateRoutine to NewRoutine, added Container page to implement DRY

---
+++ client/src/pages/NewRoutine.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+// import Header from '../components/Header'
+// import Footer from '../components/Footer';
+// import NavBar from '../components/NavBar';
+
+
+const NewRoutine = () => {
+  
+  return (
+    <div>
+stuff
+    </div>
+  );
+}
+
+export default NewRoutine
