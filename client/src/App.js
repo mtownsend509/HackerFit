c51e453cffd002ebfa7dd7ac05e04edee2d28f16
Emilia Trentham <lavernatrentham@gmail.com>
2023-03-16 18:42:32 -0700

    Add working app and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,102 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import Signup from './components/Signup';
-import Login from './components/Login';
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 import Header from "./components/Header";
-import Profile from "./pages/Profile";
+
 import Footer from "./components/Footer";
 
-// import  CreateRoutine  from "./pages/NewRoutine";
+
+// import  NewRoutine  from "./pages/NewRoutine";
 // import RoutineLogg from "./pages/RoutineLogg";
-// import CreateRoutine from "./pages/CreateRoutine";
+import Profile from "./pages/Profile";
 
 // Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+const httpLink =
+  createHttpLink({
+    uri: "/graphql",
+  });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+const authLink = setContext(
+  (_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token =
+      localStorage.getItem(
+        "id_token"
+      );
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token
+          ? `Bearer ${token}`
+          : "",
+      },
+    };
+  }
+);
 
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+const client =
+  new ApolloClient({
+    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+    link: authLink.concat(
+      httpLink
+    ),
+    cache:
+      new InMemoryCache(),
+  });
 
 function App() {
-
-
   return (
- 
-    <ApolloProvider client={client}>
-    <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="">
-          <Routes>
-            <Route 
-              path="/"
-              element={<Profile />}
-            />
-             <Route 
-                path="/login" 
-                element={<Login />}
+    <ApolloProvider
+      client={client}
+    >
+      <Router>
+        <div>
+        {/* <div className="flex-column justify-flex-start min-100-vh"> */}
+          <Header />
+          <div className="">
+            <Routes>
+
+         {/* each page route */}
+              
+              <Route
+                path="/"
+                element={
+                  <Profile />
+                }
               />
-              <Route 
-                path="/signup" 
-                element={<Signup />}
+              <Route
+                path="/signup"
+                element={
+                  <Signup />
+                }
               />
-          </Routes>
+              <Route
+                path="/login"
+                element={
+                  <Login />
+                }
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
-  </ApolloProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
