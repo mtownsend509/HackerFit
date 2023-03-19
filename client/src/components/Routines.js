dd3a424505452284fb69904ee5e1b54daa3b80e5
Kyle Feldman <flip93wolf@gmail.com>
2023-03-18 20:18:45 -0700

    Started filling out Routines component, need beeter insight of data to get farther

--- client/src/components/Routines.js
+++ client/src/components/Routines.js
@@ -1,12 +1,59 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+import {QUERY_USER} from "../utils/queries"
+import {QUERY_SINGLE_ROUTINE} from "../utils/queries";
 
 const Routines = () => {
-// use states?
-  return (
-<div>
-  Routines component
-</div>
-  );
-};
 
+
+  // set state for reps?
+  // const [repState, setRepState] = useState("");
+    // set state for sets?
+  // const [setsState, setSetsState] = useState("")
+
+  const { routineData } = useQuery(QUERY_SINGLE_ROUTINE);
+
+  const { data } = useQuery(QUERY_USER)
+
+  console.log("routines Comp line 20")
+  console.log(data)
+  let user;
+  if (data) {
+    user = data.user
+  }
+  console.log(user)
+  // const { exercise } = useQuery()
+  return (
+    <>
+      <div className="container my-1">
+
+
+        {user ? (
+          <>
+            {user.savedRoutines.map((routine) => (
+              <div key={routine._id} className="my-2">
+
+                <div className="flex-row">
+                  {routine.exercises.map(({ _id, name, muscle, instructions }, index) => (
+                    <div key={index} className="">
+                      <p>{name}</p>
+                      <p>{muscle}</p>
+                      <p>{instructions}</p>
+
+                      <div>
+
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : null}
+      </div>
+    </>
+  );
+}
 export default Routines;
--- client/src/pages/RoutineLog.js
+++ client/src/pages/RoutineLog.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Routines } from '../components/Routines'
+import { CreateRoutine } from '../components/CreateRoutine'
 
 const RoutineLog = () => {
+  const [formState, setFormState] = useState("")
   return (
-    <div>RoutineLogg</div>
+    <div>
+      <div>
+        <Routines />
+      </div>
+      <div>
+        <CreateRoutine />
+        {/*  /\ i dont think this is going to work */}
+      </div>
+    </div>
   )
 }
 
