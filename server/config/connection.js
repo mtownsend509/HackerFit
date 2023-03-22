7cb2059d094125ec31e008f85047f512e63a168a
mtownsend509 <mgtownsend509@gmail.com>
2023-03-21 19:41:05 -0700

    consolelog in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,5 +4,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hackerfit
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+console.log("Process.envmongodburi", process.env.MONGODB_URI);
 module.exports = mongoose.connection;
