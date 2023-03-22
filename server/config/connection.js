c5c351c7c32531547899deeca4fd11af9e1a0b35
mtownsend509 <mgtownsend509@gmail.com>
2023-03-21 20:59:33 -0700

    changed static paths in server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hackerfit', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hackerfit', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 console.log("Process.envmongodburi", process.env.MONGODB_URI);
 module.exports = mongoose.connection;
